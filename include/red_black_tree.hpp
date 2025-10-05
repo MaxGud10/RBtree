671763b8d72d62c26aa87e61f090a1d248226ea8
Max <karatemaksim0110@gmail.com>
2025-10-05 19:10:14 +0300

    вторая версия. уже выводит поиск элементов и добавил простенький dump_graphiz

--- include/red_black_tree.hpp
+++ include/red_black_tree.hpp
@@ -15,7 +15,8 @@ enum class Color
 template <typename KeyT>
 struct Node 
 {
-
+    // shared_ptr - смарт указатель, который позволяет нескольким shared_ptr совместо владеть один и тем же объектом
+    // то есть у нас может быть несколько указателей shared_ptr на один и тот же объект по этому адресу    
     std::shared_ptr<Node<KeyT>> parent_ = nullptr;
     std::shared_ptr<Node<KeyT>> left_   = nullptr;
     std::shared_ptr<Node<KeyT>> right_  = nullptr;
@@ -25,6 +26,33 @@ struct Node
 
     Node() = default;
     Node(KeyT key) : key_(key), color(Color::red) {}
+
+    Node(Node&& rhs) 
+    {
+        key_  = rhs.key_;
+        color = rhs.color;
+    }
+
+    Node& operator=(const Node& rhs)
+    {
+        if (this == &rhs)
+        {
+            return *this;
+        }
+
+        key_    = rhs.key_;
+        color   = rhs.color;
+        parent_ = rhs.parent_;
+        left_   = rhs,left_;
+        right_  = rhs.right_;
+
+        return *this;
+    }
+
+    // void operator%(const Node& rhs)
+    // {
+    //     std::count << "operator %\n";
+    // }
 };
 
 template <typename KeyT>
@@ -34,6 +62,7 @@ public:
     std::shared_ptr<Node<KeyT>> root_  = nullptr;
 
 private:
+    // балансировка после вставки ключа 
     void fix_insert(std::shared_ptr<Node<KeyT>> node)
     {
         while(node != root_ && node->parent_->color == Color::red)
@@ -59,6 +88,11 @@ private:
                             node = node->parent_;
                             left_rotate(node);
                         }
+
+                        node->parent_->color          = Color::black;
+                        node->parent_->parent_->color = Color::red;
+
+                        right_rotate(node->parent_->parent_);
                     }
                 }
                 
@@ -96,7 +130,7 @@ private:
     }
 
 
-    void left_rotate(std::shared_ptr<Node<KeyT>> node) 
+    void left_rotate(std::shared_ptr<Node<KeyT>> node) // TODO проверить есть ли ребенок 
     {
         auto y = node->right_;
                  node->right_ = y->left_;
@@ -118,7 +152,7 @@ private:
                    node->parent_ = y;
     }
 
-    void right_rotate(std::shared_ptr<Node<KeyT>> node) 
+    void right_rotate(std::shared_ptr<Node<KeyT>> node)  // TODO проверить есть ли ребенок 
     {
         auto y = node->left_;
                  node->left_ = y->right_;
@@ -147,6 +181,7 @@ public:
         root_->color = Color::black;
     }
 
+    // вставка ключа 
     void insert_elem(KeyT key)
     {
         auto new_node = std::make_shared<Node<KeyT>> (key);
@@ -159,7 +194,11 @@ public:
             if (key < current->key_) 
                 current = current->left_;
 
-            else current = current->right_;
+            else if (key > current->key_)
+                current = current->right_;
+
+            else 
+                return;
         }
 
         new_node->parent_ = parent;
@@ -173,12 +212,53 @@ public:
             parent->right_ = new_node;
 
         new_node->color = Color::red;
-        fix_insert(new_node);        
+        fix_insert(new_node);         // балансировка    
     }
 
-    bool serch(KeyT key1, KeyT key2) 
+    bool search(KeyT key1, KeyT key2) const
     {
-        return false;
+
+        uint64_t counter = 0;
+        //std::shared_ptr<Node<KeyT>> node = root_;
+
+        search_(root_, counter, key1, key2);
+
+        // while (node) { // для key1
+
+        //     if (key1 < *node) {
+        //         node = node->right_
+        //         counter++;
+        //     }
+        // }
+        std::cout << "counter = " << counter << std::endl;
+
+        return true;
+    }
+
+
+    void search_(std::shared_ptr<Node<KeyT>> node, uint64_t& counter, KeyT key1, KeyT key2) const 
+    {
+        if (node->key_ >= key1 && node->key_ <= key2)
+        {
+            counter++;
+            if (node->left_)
+                search_(node->left_, counter, key1, key2);
+
+            if (node->right_)
+            search_(node->right_, counter, key1, key2);
+        }
+
+        else if (node->key_ < key1 && node->right_) 
+        {
+            search_(node->right_, counter, key1, key2);
+        }
+
+        else if (node->key_ > key2 && node->left_) 
+        {
+            search_(node->left_, counter, key1, key2);
+        }
+
+        return;
     }
 
     void delete_node() 
@@ -186,23 +266,26 @@ public:
 
     }
 
-    void print(Node<KeyT>& node) 
+    void print(Node<KeyT>& node, std::ofstream& file_name) 
     {
-        std::cout << " { " << node.key_ << " color: " << static_cast<int>(node.color);
-
-        if (node.left_) 
+       // std::cout << " { " << node.key_ << " color: " << static_cast<int>(node.color);
+        if (node.left_)
         {
-            //std::cout << "\t";
-            print(*node.left_);
+            file_name 
+            << node.key_ << " [shape = Mrecord, style = filled, fillcolor = " << (static_cast<int>(node.color) == 0 ? "maroon" : "Gray") << ", label = \"" <<  node.key_ << "\" ];\n"
+            << node.left_->key_ << " [shape = Mrecord, style = filled, fillcolor = " << (static_cast<int>(node.left_->color) == 0 ? "maroon" : "Gray") << ", label = \"" << node.left_->key_ << "\" ];\n"
+            << node.key_ << " -> " << node.left_->key_ << ";\n";
+            print(*node.left_, file_name);
         }
-
         if (node.right_) 
         {
-            //std::cout << "\t";
-            print(*node.right_);
+            file_name 
+            << node.key_ << " [shape = Mrecord, style = filled, fillcolor = " << (static_cast<int>(node.color) == 0 ? "maroon" : "Gray") << ", label = \"" <<  node.key_ << "\" ];\n"
+            << node.right_->key_ << " [shape = Mrecord, style = filled, fillcolor = " << (static_cast<int>(node.right_->color) == 0 ? "maroon" : "Gray") << ", label = \"" << node.right_->key_ << "\" ];\n"
+            << node.key_ << " -> " << node.right_->key_ << ";\n";
+            print(*node.right_, file_name);
         }
-
-        std::cout << " }";
+        //std::cout << " }";
     }
 };
 
@@ -210,7 +293,6 @@ public:
 template <typename KeyT>
 class binary_tree 
 {
-
     Node<KeyT> root_;
     void print(Node<KeyT>& node) 
     {
@@ -228,9 +310,6 @@ class binary_tree
 
         std::cout << " }";
     }
-
-
 };
-
 
 };
--- main.cpp
+++ main.cpp
@@ -1,41 +1,61 @@
 #include <iostream>
 #include <cstdint>
 #include <memory>
+#include <fstream>
 
 #include "red_black_tree.hpp"
 
-int main() 
-{
-    std::cout << "start\n";
-    uint64_t key = 100;
+// что сделать еще
+// поиск элемента 
+// удаление элемента
+// сделать 5 (copy ctor, move ctor, op=, op=, dtor)
+
+int main() {
+    uint64_t key = 100; // сделать добавление в режиме реального времени
     int a = 0, b = 0;
 
+    //std::shared_ptr<Tree::Node<uint64_t>> root = std::make_shared<Tree::Node<uint64_t>>(key);
     Tree::Red_black_tree<uint64_t> rb_tree{key};
 
     std::cout << "root was created\n";
 
     char mode;
 
-    while(std::cin >> mode) 
-    {
-        if (mode == 'k') 
-        {
+    while(std::cin >> mode) {
+        if (mode == 'k') {
             std::cout << "insert\n"; 
             std::cin >> key;
             rb_tree.insert_elem(key);
         }
-
-        else if (mode == 'q') 
-        {
+        else if (mode == 'q') {
             std::cin >> a >> b;
-            rb_tree.serch(a, b);
+            if (a > b)  {
+                std::cout << "0\n";
+                break;
+            }
+            rb_tree.search(a, b);
         }
-
         else 
             break;
     }
+    //#ifndef NDEBUG
+        /*   create a tree in graphviz   */
+    std::ofstream file_graph;
+    file_graph.open("file_graph.dot");
 
-    rb_tree.print(*rb_tree.root_);
+    file_graph << "digraph RB_tree{\n"
+                  "label = < Red-black tree >;\n"
+                  "bgcolor = \"#BAF0EC\";\n"
+                  "node [shape = record ];\n"
+                  "edge [style = filled ];\n";
+   
+
+    rb_tree.print(*rb_tree.root_, file_graph);
+    file_graph << "}";
+    file_graph.close();
+    system ("dot -Tpng file_graph.dot -o tree_graph.png");
+    system ("open tree_graph.png");
+    //#endif
 
     return 0;
 }

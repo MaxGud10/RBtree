27955da9cb789651b3f4ff5d5cf972444e9d8f34
Max <karatemaksim0110@gmail.com>
2025-10-06 02:28:31 +0300

    добавил тесты и так же исправил ошибку не чтения послего вызова q

--- include/red_black_tree.hpp
+++ include/red_black_tree.hpp
@@ -28,6 +28,7 @@ struct Node
     KeyT  key_;
 
     Node() = default;
+
     Node(KeyT key) : key_(key), color(Color::red) {}
 
     Node(Node&& rhs) 
@@ -51,11 +52,6 @@ struct Node
 
         return *this;
     }
-
-    // void operator%(const Node& rhs)
-    // {
-    //     std::count << "operator %\n";
-    // }
 };
 
 template <typename KeyT>
@@ -177,35 +173,10 @@ private:
                     node->parent_ = y;
     }
 
-    mutable std::size_t nil_counter_ = 0; // уникальные имена для NULL-листьев
-
-    void emit_node_(const Node<KeyT>& node, std::ofstream& out, bool is_root) const
-    {
-        // цвета
-        const char* fill =
-            is_root ? "#5A5A5A"                  
-                    : (node.color == Color::red ? "#D85C5C"  
-                                                : "#BDBDBD"); 
-
-        const char* fontcolor = is_root ? "white" : "black";
-
-
-        std::ostringstream parent_ss, left_ss, right_ss;
-        if (auto p = node.parent_) parent_ss << p->key_; else parent_ss << "NIL";
-        if (node.left_ ) left_ss  << node.left_->key_;   else left_ss   << "NIL";
-        if (node.right_) right_ss << node.right_->key_;  else right_ss  << "NIL";
-
-        out << node.key_
-            << " [shape=Mrecord, style=filled, fillcolor=\"" << fill
-            << "\", fontcolor=\"" << fontcolor
-            << "\", label=\"{ key: " << node.key_
-            << " | parent: " << parent_ss.str()
-            << " | { L: " << left_ss.str() << " | R: " << right_ss.str()
-            << " } }\" ];\n";
-    }
-
 
 public:
+    Red_black_tree() = default;
+
     Red_black_tree(KeyT key) 
     {
         root_ = std::make_shared<Node<KeyT>> (key);
@@ -246,58 +217,76 @@ public:
         fix_insert(new_node);         // балансировка    
     }
 
-    bool search(KeyT key1, KeyT key2) const
-    {
-
+    uint64_t range_queries(KeyT key1, KeyT key2) const 
+    { 
         uint64_t counter = 0;
-        //std::shared_ptr<Node<KeyT>> node = root_;
 
-        search_(root_, counter, key1, key2);
+        search(root_, counter, key1, key2);
 
-        // while (node) { // для key1
-
-        //     if (key1 < *node) {
-        //         node = node->right_
-        //         counter++;
-        //     }
-        // }
-        std::cout << "counter = " << counter << std::endl;
-
-        return true;
+        return counter;
     }
 
 
-    void search_(std::shared_ptr<Node<KeyT>> node, uint64_t& counter, KeyT key1, KeyT key2) const 
+private:
+    void search(std::shared_ptr<Node<KeyT>> node, uint64_t& counter, KeyT key1, KeyT key2) const 
     {
         if (node->key_ >= key1 && node->key_ <= key2)
         {
             counter++;
             if (node->left_)
-                search_(node->left_, counter, key1, key2);
+                search(node->left_, counter, key1, key2);
 
             if (node->right_)
-            search_(node->right_, counter, key1, key2);
+            search(node->right_, counter, key1, key2);
         }
 
         else if (node->key_ < key1 && node->right_) 
         {
-            search_(node->right_, counter, key1, key2);
+            search(node->right_, counter, key1, key2);
         }
 
         else if (node->key_ > key2 && node->left_) 
         {
-            search_(node->left_, counter, key1, key2);
+            search(node->left_, counter, key1, key2);
         }
 
         return;
     }
+
+    std::shared_ptr<Node<KeyT>> lower_bound(KeyT key) const;
+    std::shared_ptr<Node<KeyT>> upper_bound(KeyT key) const; 
 
     void delete_node() 
     {
 
     }
 
+    mutable std::size_t nil_counter_ = 0; // уникальные имена для NULL-листьев
 
+    void emit_node_(const Node<KeyT>& node, std::ofstream& out, bool is_root) const
+    {
+        const char* fill = is_root ? "#5A5A5A"                  
+                                   : (node.color == Color::red ? "#D85C5C"  
+                                   : "#BDBDBD"); 
+
+        const char* fontcolor = is_root ? "white" : "black";
+
+
+        std::ostringstream parent_ss, left_ss, right_ss;
+        if (auto p = node.parent_) parent_ss << p->key_; else parent_ss << "NIL";
+        if (node.left_ ) left_ss  << node.left_->key_;   else left_ss   << "NIL";
+        if (node.right_) right_ss << node.right_->key_;  else right_ss  << "NIL";
+
+        out << node.key_
+            << " [shape=Mrecord, style=filled, fillcolor=\"" << fill
+            << "\", fontcolor=\"" << fontcolor
+            << "\", label=\"{ key: " << node.key_
+            << " | parent: " << parent_ss.str()
+            << " | { L: " << left_ss.str() << " | R: " << right_ss.str()
+            << " } }\" ];\n";
+    }
+
+public:
     void print(const Node<KeyT>& node, std::ofstream& out, bool is_root = false) const
     {
         emit_node_(node, out, is_root);
@@ -359,51 +348,7 @@ public:
             std::string cmd = "dot -Tpng \"" + dot_path + "\" -o \"" + png_path + "\"";
             (void)std::system(cmd.c_str());
         }
-    }
 
-    // void print(Node<KeyT>& node, std::ofstream& file_name) 
-    // {
-    //    // std::cout << " { " << node.key_ << " color: " << static_cast<int>(node.color);
-    //     if (node.left_)
-    //     {
-    //         file_name 
-    //         << node.key_ << " [shape = Mrecord, style = filled, fillcolor = " << (static_cast<int>(node.color) == 0 ? "maroon" : "Gray") << ", label = \"" <<  node.key_ << "\" ];\n"
-    //         << node.left_->key_ << " [shape = Mrecord, style = filled, fillcolor = " << (static_cast<int>(node.left_->color) == 0 ? "maroon" : "Gray") << ", label = \"" << node.left_->key_ << "\" ];\n"
-    //         << node.key_ << " -> " << node.left_->key_ << ";\n";
-    //         print(*node.left_, file_name);
-    //     }
-    //     if (node.right_) 
-    //     {
-    //         file_name 
-    //         << node.key_ << " [shape = Mrecord, style = filled, fillcolor = " << (static_cast<int>(node.color) == 0 ? "maroon" : "Gray") << ", label = \"" <<  node.key_ << "\" ];\n"
-    //         << node.right_->key_ << " [shape = Mrecord, style = filled, fillcolor = " << (static_cast<int>(node.right_->color) == 0 ? "maroon" : "Gray") << ", label = \"" << node.right_->key_ << "\" ];\n"
-    //         << node.key_ << " -> " << node.right_->key_ << ";\n";
-    //         print(*node.right_, file_name);
-    //     }
-    //     //std::cout << " }";
-    // }
-};
-
-
-template <typename KeyT>
-class binary_tree 
-{
-    Node<KeyT> root_;
-    void print(Node<KeyT>& node) 
-    {
-        std::cout << " { " << node.key_;
-
-        if (node.left_) 
-        {
-            print(*node.left_);
-        }
-
-        if (node.right_) 
-        {
-            print(*node.right_);
-        }
-
-        std::cout << " }";
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,61 +1,84 @@
 #include <iostream>
 #include <cstdint>
-#include <memory>
 #include <fstream>
+#include <string>
+#include <set>
 
 #include "red_black_tree.hpp"
 
-// что сделать еще
-// поиск элемента 
-// удаление элемента
-// сделать 5 (copy ctor, move ctor, op=, op=, dtor)
 
-int main() {
-    uint64_t key = 100; // сделать добавление в режиме реального времени
-    int a = 0, b = 0;
+static int64_t range_queries_set(const std::set<int64_t>& set, int64_t a, int64_t b);
 
-    //std::shared_ptr<Tree::Node<uint64_t>> root = std::make_shared<Tree::Node<uint64_t>>(key);
-    Tree::Red_black_tree<uint64_t> rb_tree{key};
+int main() 
+{
+    #ifdef SET_MODE_ENABLED
+        std::set<int64_t> set; 
+    #endif
 
-    std::cout << "root was created\n";
+    uint64_t key = 0; 
+    char     mode;
 
-    char mode;
+    Tree::Red_black_tree<uint64_t> rb_tree;
 
-    while(std::cin >> mode) {
-        if (mode == 'k') {
-            std::cout << "insert\n"; 
-            std::cin >> key;
+    int64_t a = 0;
+    int64_t b = 0;
+
+    while((std::cin >> mode).good()) 
+    {
+        if (mode == 'k') 
+        {
+            if (!(std::cin >> key).good())
+                break;
+
+            #ifdef SET_MODE_ENABLED
+                set.insert(key); 
+            #endif
             rb_tree.insert_elem(key);
         }
-        else if (mode == 'q') {
-            std::cin >> a >> b;
-            if (a > b)  {
-                std::cout << "0\n";
-                break;
+
+        else if (mode == 'q') 
+        {
+            if (!(std::cin >> a >> b).good())
+                continue;
+            if (a > b)  
+            {
+                std::swap(a, b);
+
+                #ifdef SET_MODE_ENABLED
+                    std::cout << "set = " << "0 " << ' ';
+                #endif 
+
+                continue;
             }
-            rb_tree.search(a, b);
+
+            int64_t counter = rb_tree.range_queries(a, b);
+            std::cout << counter << ' ';
+
+            #ifdef SET_MODE_ENABLED
+                int64_t counter_set = range_queries_set(set, a, b);
+                std::cout << "set = " << counter_set << ' ';
+            #endif 
         }
+
         else 
             break;
     }
-    //#ifndef NDEBUG
-        /*   create a tree in graphviz   */
-    std::ofstream file_graph;
-    file_graph.open("file_graph.dot");
 
-    file_graph << "digraph RB_tree{\n"
-                  "label = < Red-black tree >;\n"
-                  "bgcolor = \"#BAF0EC\";\n"
-                  "node [shape = record ];\n"
-                  "edge [style = filled ];\n";
-   
+    std::cout << std::endl;
 
-    rb_tree.print(*rb_tree.root_, file_graph);
-    file_graph << "}";
-    file_graph.close();
-    system ("dot -Tpng file_graph.dot -o tree_graph.png");
-    system ("open tree_graph.png");
-    //#endif
+    #ifndef NDEBUG
+        rb_tree.dump();
+    #endif
 
     return 0;
+}
+
+int64_t range_queries_set(const std::set<int64_t>& set, int64_t a, int64_t b) 
+{
+    auto l1 = set.lower_bound(a);
+    auto l2 = set.upper_bound(b);
+    if (std::distance(set.begin(), l1) > std::distance(set.begin(), l2))
+        std::swap(l1, l2);
+
+    return std::distance(l1, l2);
 }
